a4fccbcfb9579a92cc2f98063f6d660eef4997ff
Generosa Litton <glcodeworks@gmail.com>
2024-07-08 22:05:19 -0500

    short circuit practice

--- practice.js
+++ practice.js
@@ -30,30 +30,30 @@ logging a, b, and c results in [["foo", "bar", "qux"], "baz"] twice and ["foo", 
 
 // What is the value logged on the last three lines? What concept is demonstrated?
 
-const a = [["foo", "bar"], "baz"];
-const b = a;
-a[1] = a[1].toUpperCase();
-let c = b[0];
-c.push("qux");
+// const a = [["foo", "bar"], "baz"];
+// const b = a;
+// a[1] = a[1].toUpperCase();
+// let c = b[0];
+// c.push("qux");
 
-console.log(a); //[ [ 'foo', 'bar', 'qux' ], 'BAZ' ]
-console.log(b); //[ [ 'foo', 'bar', 'qux' ], 'BAZ' ]
-console.log(c); //[ 'foo', 'bar', 'qux' ]
+// console.log(a); //[ [ 'foo', 'bar', 'qux' ], 'BAZ' ]
+// console.log(b); //[ [ 'foo', 'bar', 'qux' ], 'BAZ' ]
+// console.log(c); //[ 'foo', 'bar', 'qux' ]
 
 //  What will be logged to the console, and why?
 
-function arrayModifier(arr) {
-  arr[0] = 100; //[100, 2, 3, 11]
-  // console.log("array", arr);
-  arr = arr.filter((num) => num > 10); //[100, 11]
-  console.log("filter", arr);
-  arr.push(200); ////[100, 11, 200]
-  console.log("push", arr);
-}
+// function arrayModifier(arr) {
+//   arr[0] = 100; //[100, 2, 3, 11]
+//   // console.log("array", arr);
+//   arr = arr.filter((num) => num > 10); //[100, 11]
+//   // console.log("filter", arr);
+//   arr.push(200); ////[100, 11, 200]
+//   // console.log("push", arr);
+// }
 
-const originalArray = [1, 2, 3, 11];
-arrayModifier(originalArray);
-console.log(originalArray); //[100, 2, 3, 11];
+// const originalArray = [1, 2, 3, 11];
+// arrayModifier(originalArray);
+// console.log(originalArray); //[100, 2, 3, 11];
 
 /*
 The output is [100, 2, 3, 11].
@@ -73,18 +73,68 @@ in this case.
 
 // Identify the variables, primitives, and objects in the code
 
-const person = {
-  name: "Alice",
-  age: 30,
-};
+// const person = {
+//   name: "Alice",
+//   age: 30,
+// };
 
-function updateAge(obj) {
-  obj = { name: "Alice", age: 31 };
-}
+// function updateAge(obj) {
+//   obj = { name: "Alice", age: 31 };
+// }
 
-updateAge(person);
+// updateAge(person);
 
 //Variables: person, function updateAge, obj parameter
 //Primitives: "Alice", 30 in the person object, "Alice", 31 in the updateAge
 // function
 // objects: person, function updateAge, obj
+
+// const original = {
+//   a: 1,
+//   b: { c: 2 },
+// };
+
+// const shallowCopy = { ...original };
+// shallowCopy.a = 10;
+// shallowCopy.b.c = 20;
+
+// console.log(original.a, original.b.c); // 1, 2
+// console.log(shallowCopy.a, shallowCopy.b.c); //10,20
+
+/*
+The console logs 1, 2 and 10, 20
+
+An variable original is initialized as an object and then a shallowCopy variable
+is initialized assigning its value to original using the spread operator which assigns
+the key-value pair of the object that was initialized in line 92.   Lines 98 and 99
+show the keys a and b.c being reassigned to different values than the original object.
+
+Thus logging original.a and original.b.c logs the values of the original object 1, 2 
+while shallowCopy.a and shallowCopy.b.c log values of the reassigned values.  
+
+This code demonstrates that primitive values are immutable and reassigning an object
+changes the key-value pair and not the primitive value itself.  
+
+*/
+
+let status = "offline";
+let mode = status === "offline" || "online";
+
+if (mode === "online") {
+  console.log("System is online");
+} else {
+  console.log("System is offline");
+}
+
+/*
+The output is "System is online"
+
+A status variable is initialized an assigned to the string "online".  Another
+variable mode is assigned to status which uses the strict equality operator to 
+evaluate whether status is "offline" or "online" using the logical OR operator.  
+The variable mode will equal the first truthy value of status.  Since status is
+assigned to online, mode will be assigned the string "online".
+
+As a result, the if condition will be true for mode === "online" and logs
+"System is online".
+*/
